3656bf068c25b070f25504322ff1c1e1308ac9fb
SandeepVattapparambil <sandeepv68@gmail.com>
2014-07-19 12:46:35 +0530

    tabs updates

--- scrape_home.php
+++ scrape_home.php
@@ -72,15 +72,15 @@ else
       <div class="col-lg-12"> <br>
         <div class="btn-group">
           <ul class="nav nav-tabs" role="tablist">
-  <li class="active"><a href="#home" role="tab" data-toggle="tab">Home</a></li>
-  <li><a href="#profile" role="tab" data-toggle="tab">Profile</a></li>
-  <li><a href="#messages" role="tab" data-toggle="tab">Messages</a></li>
-  <li><a href="#settings" role="tab" data-toggle="tab">Settings</a></li>
-</ul>
+            <li class="active"><a href="#home" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-globe"></span>Hyperlinks</a></li>
+            <li><a href="#profile" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-picture"></span>Images</a></li>
+            <li><a href="#messages" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-edit"></span>Custom</a></li>
+          </ul>
+          <br>
         </div>
-        <hr>
         <style>
-	  .limit{
+	  .limit
+	  {
 		width: 500px;
 		font-weight: 300;
 		white-space: nowrap;
@@ -89,8 +89,49 @@ else
 		text-align:left;
 	  }
 	  </style>
-        <p id="disp"> 
-          
+        <p id="disp">
+         <!-- Tab panes -->
+<div class="tab-content">
+  <div class="tab-pane active" id="home">
+  <?php
+  if (@file_get_contents($url))
+	{
+		echo "<h4 class=\"pull-left\">The following hyperlinks were found !</h4></p><br />";
+		$html = file_get_html($url);
+		echo "<br>";
+		echo "<table class=\"table table-hover\">";
+		echo "<tbody>";
+		/*foreach($html->find('a') as $e) 
+		echo "<tr style=\"text-align:left\"><td><strong>".$e->plaintext."</strong>
+		</td><td><a href=".$e->href."><div class=\"limit\" >".$e->href."</div>
+		</a></td><td><form action=\"scrape_home.php\" method=\"POST\">
+		<input type=\"textfield\" style=\"display:none;\" value=".$e->href." name=\"url\">
+		<button type=\"submit\" class=\"btn btn-warning btn-xs\">
+		<span class=\"glyphicon glyphicon-share\"></span> Open in new window</button>
+		</form></td></tr>";*/
+		foreach($html->find('a') as $e) 
+		echo "<tr style=\"text-align:left\"><td><strong>".$e->plaintext."</strong>
+		</td><td><a href=".$e->href."><div class=\"limit\" >".$e->href."</div>
+		</a></td><td>
+		<a type=\"button\" class=\"btn btn-warning btn-xs\" href=\"scrape_home.php?url=".$e->href."\" target=\"_blank\">
+		<span class=\"glyphicon glyphicon-share\"></span> Open in new window</a>
+		</td></tr>";
+		echo "</tbody>";
+		echo "</table>";	 
+	}
+	else
+	{
+		echo"<div class=\"alert alert-danger alert-dismissible\" role=\"alert\">
+		<button type=\"button\" class=\"close\" data-dismiss=\"alert\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span></button>
+		<strong>Oops!</strong> Better check the URL, its not looking too good.
+		</div>";
+	}
+	?>
+  </div>
+  <div class="tab-pane" id="profile">...</div>
+  <div class="tab-pane" id="messages">...</div>
+  <div class="tab-pane" id="settings">...</div>
+</div>
       </div>
     </div>
   </div>
